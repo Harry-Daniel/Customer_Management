d7d5a0c3a665597ef579eb25a797c85f69ed3eb6
Harry-Daniel <hayiborkomla@gmail.com>
2024-12-30 23:14:19 +0000

    Image File Upload to User Profile Model

--- crm/settings.py
+++ crm/settings.py
@@ -126,6 +126,8 @@ MEDIA_URL ='images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
